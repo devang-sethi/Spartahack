a1dba754ed6b0491eaa5372695e05b0900519f66
smallfie <122250169+smallfie@users.noreply.github.com>
2024-01-27 21:08:00 -0500

    Update home_script.js

--- home_script.js
+++ home_script.js
@@ -20,19 +20,19 @@ async function getRecListings() {
 
 function displayListing(obj, index, array) {
   //create div for listing
-  listing = document.createElement("div");
+  let listing = document.createElement("div");
 
   //create h3 for service title
-  service_title = document.createElement("h3");
+  let service_title = document.createElement("h3");
   service_title.textContent = obj.service_title; //set text content of service title
 
   //create img for image
-  image = document.createElement("img");
+  let image = document.createElement("img");
   image.src = obj.image_src; //set image src
   image.alt = obj.image_alt; //set image alt
 
   //create p for service description
-  service_description = document.createElement("p");
+  let service_description = document.createElement("p");
   service_description.textContent = obj.service_description; //set text content of service description
 
   //append elements to listing
